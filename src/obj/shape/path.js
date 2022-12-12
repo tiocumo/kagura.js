f13cdc2abc2c64c485131f379adca18526a7bb5a
nakasyou <79000684+nakasyou@users.noreply.github.com>
2022-12-12 07:53:54 +0000

    indent

--- src/obj/shape/path.js
+++ src/obj/shape/path.js
@@ -1,19 +1,19 @@
 class extends kagura.obj.shape.GraphicsFill{
-      constructor(options){
+  constructor(options){
         //-, 0, -100, 70, -70,
-        options=objSafe({
-          paths:[[100,0],[70,70],[0,100],[-70,70],[-100,0],[-70,-70],[0,-100],[70,-70]]
-        },options);
-        super(options);
-        let paths=[];
-        for(let path of options.paths){
-          if(path.length===2&&Array.isArray(path)){
-            paths.push(path[0],path[1]);
-          }else{
-            console.error("Paths's arguments type is array, and children array's length is 2.");return;
-          }
-        }
-        this.drawPolygon(paths).endFill();
-        
+    options=objSafe({
+      paths:[[100,0],[70,70],[0,100],[-70,70],[-100,0],[-70,-70],[0,-100],[70,-70]]
+    },options);
+    super(options);
+    let paths=[];
+    for(let path of options.paths){
+      if(path.length===2&&Array.isArray(path)){
+        paths.push(path[0],path[1]);
+      }else{
+        console.error("Paths's arguments type is array, and children array's length is 2.");return;
       }
     }
+    this.drawPolygon(paths).endFill();
+        
+  }
+}
